5b6881f3ba2f840b04614303f2cf6ae54db29ad4
Riefqi <riefqihafiz4@gmail.com>
2024-07-07 15:00:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function showGanjil() {
+    document.getElementById("iframe-ganjil").style.display = "block";
+    document.getElementById("iframe-genap").style.display = "none";
+}
+
+function showGenap() {
+    document.getElementById("iframe-ganjil").style.display = "none";
+    document.getElementById("iframe-genap").style.display = "block";
+}
+function logout() {
+    window.location.href = 'login.html'; 
+}
+
+
+
